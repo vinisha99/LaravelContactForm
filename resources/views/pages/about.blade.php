ecf00756e518751e062057d55ac2b5153aaa08d3
vinisha <vinisha99@gmail.com>
2018-10-24 01:26:48 -0400

    Fix: Updated contents

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
 
-        <h1>This is my about page</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br>
-             All you get is this text and a mostly barebones HTML document.</p>
-
-
+        <h1>This is the about page</h1>
 
 @endsection
