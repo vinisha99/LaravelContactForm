0b08c9e7f17e43c14816e36d89d64bdd11a1f6b2
vinisha <vinisha99@gmail.com>
2018-10-23 23:51:02 -0400

    Task: Added contact.blade.php file in Pages Folder

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,32 @@
+@extends('Layouts.default')
+
+@section('content')
+
+    <div class="card mt-3 pl-2 pr-2">
+
+        <div class="card-title">
+            <h1>Contact page</h1>
+            <p class="lead">This is a contact form</p>
+        </div>
+
+        <div class="card-body">
+            <form action="/contact" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="body">Message</label>
+                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary nb-2">Submit</button>
+            </form>
+        </div>
+    </div>
+
+
+
+@endsection
