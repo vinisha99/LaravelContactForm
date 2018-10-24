8d1f2608fa39c6e12a2dd0a200919e91a02bde37
vinisha <vinisha99@gmail.com>
2018-10-24 01:26:19 -0400

    Fix: Updated fields and parameters in form tag to send data

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="card mt-3 pl-2 pr-2">
+    <div class="card mt-5 pl-2 pr-2">
 
         <div class="card-title">
             <h1>Contact page</h1>
@@ -10,19 +10,24 @@
         </div>
 
         <div class="card-body">
-            <form action="/contact" method="post">
+            <form action="{{route('contact.store')}}" id="contact-form" method="POST">
                 {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
-                </div>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="given names">
+                    </div>
 
-                <div class="form-group">
-                    <label for="body">Message</label>
-                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
-                </div>
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                    </div>
 
-                <button type="submit" class="btn btn-primary nb-2">Submit</button>
+                    <div class="form-group">
+                        <label for="body">Message</label>
+                        <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary nb-2">Submit</button>
             </form>
         </div>
     </div>
